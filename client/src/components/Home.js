bffd62291537d63f5f93ca4d074feeb8f8ee7e9c
khristine-g <githigekhristine@gmail.com>
2024-02-20 22:20:08 +0300

    Adding the More component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import More from './More';
 import '../Home.css';
 
 const Home = () => {
   return (
+    <>
     <div className='home-container'>
     
     <div className='home-body'>
@@ -11,9 +13,16 @@ const Home = () => {
       <h1 className='home-header'>Hello there,welcome to Figmol</h1>
 
       <p className='home-content'>We are your website for all your biochemistry needs.
-      We have a team of amazing biochemists</p>
+      We have a team of amazing biochemists.We are your website for all your biochemistry needs.
+      We have a team of amazing biochemists.</p>
+      <p className='home-content'>We are your website for all your biochemistry needs.
+      We have a team of amazing biochemists.We are your website for all your biochemistry needs.
+      We have a team of amazing biochemists.</p>
+      <p className='home-content'>We are your website for all your biochemistry needs.
+      We have a team of amazing biochemists.We are your website for all your biochemistry needs.
+      We have a team of amazing biochemists.</p>
 
-      <Link className='title-link' to=" /quizzes/titles">See More</Link>
+      <Link className='title-link' to="/quizzes/titles">See More</Link>
      
        
 
@@ -25,6 +34,13 @@ const Home = () => {
       {/* <img src = " https://i0.wp.com/www.differencebetween.com/wp-content/uploads/2011/08/Difference-Between-School-and-Education_Education.jpg"alt="education-img" /> */}
       </div>
     </div>
+
+    <More />
+
+  
+
+    </>
+    
   );
 };
 
