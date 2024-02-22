1caf2e084c0a671c111fecb8febb7c31bceff4ed
khristine-g <githigekhristine@gmail.com>
2024-02-22 11:59:36 +0300

    Adding is admin in user model and auth controller

--- server/app/controllers/auth_controller.rb
+++ server/app/controllers/auth_controller.rb
@@ -1,29 +1,38 @@
 class AuthController < ApplicationController
-    def signup
-        user = User.new(user_params)
-    
-        if user.save
-          token = user.generate_jwt
-          render json: { token: token }
-        else
-          render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
-        end
-      end
-    def login
-      user = User.find_by(email: params[:email])
-  
-      if user && user.authenticate(params[:password])
+  def signup
+    existing_user = User.find_by(email: user_params[:email])
+
+    if existing_user
+      render json: { error: 'A user with this email already exists.' }, status: :unprocessable_entity
+    else
+      user = User.new(user_params)
+
+      # Set is_admin to false by default
+      user.is_admin = false
+
+      if user.save
         token = user.generate_jwt
         render json: { token: token }
       else
-        render json: { error: 'Invalid email or password' }, status: :unauthorized
+        render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
       end
     end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      token = user.generate_jwt
+      render json: { token: token }
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-    # has_many :quizzes
+
 
     has_secure_password
   
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     validates :password_confirmation, presence: true, on: :create
+
+
+
+    attribute :is_admin, :boolean, default: false
 
 
     def generate_jwt
